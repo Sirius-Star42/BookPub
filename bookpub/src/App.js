74176bc9b3c4344ccc0a1a323236dd8ae83690c5
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-12-08 22:23:06 +0300

    added meterial ui and getting api data

--- bookpub/src/App.js
+++ bookpub/src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {Card} from './Components/Card';
 import './App.css';
-import {Card} from './Components/Card'
+
+
+
 function App() {
+  const [bookData, setbookData] =useState([]);
+
+  const fetchBooks = () => {
+    axios.get("https://api.nytimes.com/svc/books/v3/lists/overview.json?published_date=2012-12-08&api-key=7hfOFGALLGSAwSOPuN3R2ClK1XtuMLhG")
+    .then(response => {
+      setbookData(response.data.results.lists)
+      
+    })
+    .catch(err=> {
+      console.log(err)
+    })
+  };
+
+  useEffect(()=> {
+    fetchBooks()
+  }, [])
+
+ console.log(bookData)
+ 
   return (
     <div className="Container">
+
       
-      <Card />
+      {bookData.map((book) => <Card image={book}/>
+      
+        
+        )}
       
     </div>
   );
--- bookpub/src/Components/Card.js
+++ bookpub/src/Components/Card.js
@@ -1,5 +1,16 @@
-export const Card = () => {
+
+
+
+export const Card = (props) => {
     return (
-        <p>Merhaba</p>
+        <div>
+            
+            <img src={props.image.list_image} alt="image" width={props.image.list_image_width} height={props.image.list_image_height} />
+            <p>{props.title}</p>
+            
+        </div>
+   
     )
 }
+
+
