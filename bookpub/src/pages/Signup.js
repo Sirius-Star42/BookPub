512b3b08b6b1e509d5d1ff9bf2566c6a4b6d3255
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-12-13 20:11:04 +0300

    added formik package and used for signup

--- bookpub/src/pages/Signup.js
+++ bookpub/src/pages/Signup.js
@@ -1,12 +1,79 @@
 import React from 'react';
+import { Button, Container, Grid, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useFormik  } from "formik";
+import { Email } from '@material-ui/icons';
+
+const styles = makeStyles({
+    wrapper: {
+        marginTop: '5rem',
+    }
+});
 
 function Signup() {
+    const formik = useFormik({
+        initialValues: {
+            displayName: "",
+            email: "",
+            password: "",
+        },
+        onSubmit: (value) => {
+            alert(JSON.stringify(value, null, 2));
+        },
+    });
+
+    const signupStyles = styles();
     return (
-        <div>
-
-
-
-        </div>
+        <Container className={signupStyles.wrapper} maxWidth="sm">
+            <form onSubmit={formik.handleSubmit}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <TextField 
+                        name="displayName" 
+                        label="Display Name" 
+                        variant="outlined" 
+                        fullWidth
+                        value={formik.values.displayName}
+                        onChange={formik.handleChange}  
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField 
+                        name="email" 
+                        label="Email" 
+                        variant="outlined" 
+                        fullWidth
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        type= "email"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField 
+                        name="password" 
+                        label="Password" 
+                        variant="outlined" 
+                        fullWidth
+                        value={formik.values.password}
+                        onChange={formik.handleChange}  
+                        type="password"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button 
+                        type="submit" 
+                        variant="contained"
+                        color="primary" 
+                        fullWidth>
+                            Submit
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" fullWidth>Signup with Google</Button>
+                </Grid>
+            </Grid>
+            </form>
+        </Container>
     )
     
 }
